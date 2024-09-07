da6bbd5770d61baead82f743bceb92e543191aae
Nadechda <nadechda-s@yandex.ru>
2024-09-07 11:02:10 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as d_0
+from true_math import divide as d_inf
+result1 = d_0(69, 3)
+result2 = d_0(3, 0)
+result3 = d_inf(49, 7)
+result4 = d_inf(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
